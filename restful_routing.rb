46e767bc30d8068e04666dd559af096bdeddacfa
Isaac Toleafoa <isaactoleafoa@gmail.com>
2016-12-23 17:36:31 -0700

    completed the settings page added all fields

---
+++ restful_routing.rb
@@ -0,0 +1,18 @@
+                  Prefix Verb   URI Pattern                    Controller#Action
+              home_index GET    /home/index(.:format)          home#index
+                    root GET    /                              home#index
+        new_user_session GET    /users/sign_in(.:format)       users/sessions#new
+            user_session POST   /users/sign_in(.:format)       users/sessions#create
+    destroy_user_session DELETE /users/sign_out(.:format)      users/sessions#destroy
+       new_user_password GET    /users/password/new(.:format)  devise/passwords#new
+      edit_user_password GET    /users/password/edit(.:format) devise/passwords#edit
+           user_password PATCH  /users/password(.:format)      devise/passwords#update
+                         PUT    /users/password(.:format)      devise/passwords#update
+                         POST   /users/password(.:format)      devise/passwords#create
+cancel_user_registration GET    /users/cancel(.:format)        users/registrations#cancel
+   new_user_registration GET    /users/sign_up(.:format)       users/registrations#new
+  edit_user_registration GET    /users/edit(.:format)          users/registrations#edit
+       user_registration PATCH  /users(.:format)               users/registrations#update
+                         PUT    /users(.:format)               users/registrations#update
+                         DELETE /users(.:format)               users/registrations#destroy
+                         POST   /users(.:format)               users/registrations#create
